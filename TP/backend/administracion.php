be385dfe609656d4823bbf94dd3e6c5a5c40594b
Matías B Crespo <matisfd@gmail.com>
2021-09-16 12:05:10 -0300

    Agrego subida de archivos al servidor y validación

--- TP/backend/administracion.php
+++ TP/backend/administracion.php
@@ -10,6 +10,7 @@ $fabrica->SetCantidadMaxima(7);
 $fabrica->TraerDeArchivo('./archivos/empleados.txt');
 $cantidadPrevia = $fabrica->GetCantidadEmpleados();
 
+// EMPLEADO
 //$apellido, $nombre, $dni, $sexo, $legajo, $sueldo, $turno
 $apellido = isset($_POST['txtApellido']) ? $_POST['txtApellido'] : null;
 $nombre = isset($_POST['txtNombre']) ? $_POST['txtNombre'] : null;
@@ -18,6 +19,49 @@ $sexo = isset($_POST['cboSexo']) ? $_POST['cboSexo'] : null;
 $legajo = isset($_POST['txtLegajo']) ? $_POST['txtLegajo'] : null;
 $sueldo = isset($_POST['txtLegajo']) ? $_POST['txtLegajo'] : null;
 $turno = isset($_POST['rdoTurno']) ? $_POST['rdoTurno'] : null;
+
+//Imagen
+$upload = false;
+$esImage = false;
+$extension = "";
+$file = isset($_FILES['txtFoto']) ? $_FILES['txtFoto'] : null;
+$tmpName = isset($_FILES['txtFoto']) ? $_FILES['txtFoto']['tmp_name'] : null;
+
+//Destino auxiliar
+$destinoAux = './fotos/' . $_FILES['txtFoto']['name'];
+$destinoFinal = './fotos/' . "$dni-$apellido.".pathinfo($destinoAux,PATHINFO_EXTENSION);
+
+
+if($file !== null)
+{
+    if(file_exists($destinoFinal))
+    {
+        //el archivo ya existe
+    }
+    else
+    {
+        //genero validaciones
+        if($file['size']<1000000)
+        {
+            $esImage = getimagesize($file['tmp_name']);
+            if($esImage)
+            {
+                $extension=pathinfo($destinoAux, PATHINFO_EXTENSION);
+                switch($extension)
+                {
+                    case 'jpg':
+                    case 'bmp':
+                    case 'gif':
+                    case 'png':
+                    case 'jpeg':
+                        if(move_uploaded_file($tmpName,$destinoFinal))
+                        $upload = true;
+                }
+            }
+        }
+    }
+}
+
 
 //Guardar en archivo
 $newEmpleado = new Empleado($apellido,$nombre,$dni,$sexo,$legajo,$sueldo,$turno);
